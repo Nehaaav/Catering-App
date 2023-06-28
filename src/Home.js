43a86a13cfbef732b7ff649e8365aa90d387e015
Nehaaav <nehavinay1702@gmail.com>
2023-06-28 09:08:05 +0530

    Version 7

--- src/Home.js
+++ src/Home.js
@@ -11,7 +11,7 @@ const Home = (props) => {
         <Background img={require('./assets/food4.webp')} backgroundImgHeight={'100%'} imgOpacity={0.35}>
             <View style={{marginHorizontal:10,marginVertical:420}}>
                 <Text style={{fontSize:18,color:'#FEF6E1',fontWeight:'bold',height:30}}>ವಾಟೆತ್ತಿಲ ಕ್ಯಾಟರಿಂಗ್</Text>
-                <Text style={{fontSize:50,color:'#FEF6E1',fontWeight:'bold',marginTop:5,height:70}}>ಸ್ವಾಗತ!</Text>
+                <Text style={{fontSize:50,color:'#FEF6E1',fontWeight:'bold',marginTop:5,height:80}}>ಸ್ವಾಗತ!</Text>
                 <Text style={{fontSize:13,color:'#FEF6E1',fontWeight:'bold',marginTop:15,height:50}}>ನಮ್ಮ ಅಡುಗೆ ಸೇವೆಗಳೊಂದಿಗೆ ನಿಮ್ಮ ಕಡುಬಯಕೆಗಳನ್ನು ಪೂರೈಸಿಕೊಳ್ಳಿ!</Text>
                 <Btn bgColor={'#A0002C'} textColor={'#FEF6E1'} btnLabel={"ಪ್ರಾರಂಭಿಸಿ!"} btnwidth={330} btnHeight={51} txtmargin={10} BtnMgTop={40} Press={() => props.navigation.navigate("Login")}/>
             </View>
